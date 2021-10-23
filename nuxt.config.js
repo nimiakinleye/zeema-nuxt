f2498947bb542fea83d01cdef6ad2a62d0168986
nimiakinleye <toluwanimiakinleye01@gmail.com>
2021-10-23 15:10:11 +0100

    added description, favicon and scroll to top

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'zeema-fcu',
+    title: 'Zeema Federal Credit Union',
     htmlAttrs: {
       lang: 'en'
     },
@@ -12,14 +12,17 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description', name: 'description', content: `We're a member-owned and not-for-profit credit union exclusively serving America’s immigrant community and their families. Our members are owners, not just customers, and each one of them has a vote.
+      Because we’re not-for-profit, any surplus funds are returned to our members as dividends, reduced loan interest and improvements to our products and services. That means we have great rates, lower fees and exclusive discounts.` },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', type: 'text/css', href: '~/assets/css/main.css' },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      { rel: "preconnect", href: "https://fonts.gstatic.com"},
-      { rel:"stylesheet", href:"https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap", rel: "stylesheet" }
+      { rel: "preconnect", href: "https://fonts.gstatic.com" },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap", rel: "stylesheet" }
     ]
   },
 
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,40 @@
     <Benefits />
     <Subscribe />
     <AppFooter />
+    <div @click="toTop" :class="{show: scroll, hide:!scroll }" class="scroll_up">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+        <rect width="256" height="256" fill="none" />
+        <circle
+          cx="128"
+          cy="128"
+          r="96"
+          fill="none"
+          stroke="#00803d"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        />
+        <polyline
+          fill="none"
+          stroke="#00803d"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+          points="94.059 121.941 128 88 161.941 121.941"
+        />
+        <line
+          x1="128"
+          x2="128"
+          y1="168"
+          y2="88"
+          fill="none"
+          stroke="#00803d"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="24"
+        />
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -28,5 +62,52 @@ export default {
     Subscribe,
     AppFooter,
   },
+
+  data() {
+    return {
+      scroll: false
+    }
+  },
+
+  methods: {
+    onScroll() {
+      if(window.pageYOffset > 50) {
+        this.scroll = true
+      } else {
+        this.scroll = false
+      }
+    },
+    toTop() {
+      document.body.scrollTop = 0 // For Safari
+      document.documentElement.scrollTop = 0 // For Chrome
+    }
+  },
+
+  beforeMount() {
+    window.addEventListener('scroll', this.onScroll)
+  }
 }
 </script>
+
+<style scoped>
+.scroll_up {
+  width: 50px;
+  position: fixed;
+  bottom: 30px;
+  right: 40px;
+  transition: 1000ms;
+  cursor: pointer;
+}
+
+.scroll_up svg {
+  border-radius: 100%;
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+.hide {
+  transform: translateY(1000%);
+}
+.show {
+  transform: translateY(0);
+}
+</style>
